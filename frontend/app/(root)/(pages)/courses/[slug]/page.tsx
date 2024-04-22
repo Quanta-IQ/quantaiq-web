6fb84df6351df76d4cbbd09baca6ee5f645aa740
Charles Calapini <7816568+calapsss@users.noreply.github.com>
2024-04-22 08:27:12 -0600

    Update page.tsx
    Course Page

--- frontend/app/(root)/(pages)/courses/[slug]/page.tsx
+++ frontend/app/(root)/(pages)/courses/[slug]/page.tsx
@@ -1,44 +1,62 @@
+"use client";
+import {useQuery} from "convex/react";
 import Image from "next/image";
 import React from "react";
 import Search from "@/components/ui/search";
 import {Button} from "@/components/ui/button";
 import Link from "next/link";
-import CourseCard from "../../../../components/course/course-card";
-import CourseFeed from "../../../../components/course/course-feed";
+import { api } from "@/convex/_generated/api"
+import { Id } from "../../../../../convex/_generated/dataModel";
+import {Separator} from "@/components/ui/separator";
 
 export default function Page({params} : {params: {slug: string}}) {
-  return (
-    <main className="flex min-h-screen flex-col p-24 pl-48 pr-6">
-      <div className="mb-4 mt-4 ml-4">
-        <div className="flex flex-row justify-between">
-          <div>
-            <h1 className="text-4xl font-bold">Courses</h1>
-            <p className="text-gray-500 "> Find your next course</p>
-          </div>
-          <div className="items-center pt-2">
-            <Button  >
-              <Link href="/courses/create">
-              Create Course
-              </Link>
-            </Button>
-          </div>
-        </div>
-        
-        <div className="mt-3 mb-3"> 
-          <Search placeholder="TODO: Make this functional" />
-        </div>
-        <div className="mt-3">
-            <CourseFeed />
-        </div>
-        
-      </div>
-      
-      
 
-      
-
-
-
-    </main>
-  );
+    const courseInfo = useQuery(api.functions.courses.getCourseByCourseID, {
+        CourseID: params.slug as Id<"Courses">
+    }) 
+    console.log(courseInfo);
+    return (
+      <>
+        {courseInfo && (
+          <main className="flex min-h-screen flex-col p-24 pl-48 pr-6">
+            <div className="mb-4 mt-4 ml-4">
+              <div className="flex flex-row justify-between pb-6">
+                <div>
+                  <h1 className="text-4xl font-bold">{courseInfo.CourseName}</h1>
+                  <p className="text-gray-500 "> {courseInfo.CourseDescription}</p>
+                </div>
+              </div>
+              <Separator />
+              <div className="flex flex-row items-center pt-6 gap-4 ">
+                <Button>
+                  <Link href={`/courses/${courseInfo._id}/syllabus`}>
+                    Syllabus
+                  </Link>
+                </Button>
+                <Button>
+                  <Link href={`/courses/${courseInfo._id}/test`}>
+                    Test
+                  </Link>
+                </Button>
+                <Button>
+                  <Link href={`/courses/${courseInfo._id}/class`}>
+                    Classes
+                  </Link>
+                </Button>
+                <Button>
+                  <Link href={`/courses/${courseInfo._id}/ai`}>
+                    AI Tools
+                  </Link>
+                </Button>
+                <Button>
+                  <Link href={`/courses/${courseInfo._id}/edit`}>
+                    Edit Details
+                  </Link>
+                </Button>
+              </div>
+            </div>
+          </main>
+        )}
+      </>
+    );
 }

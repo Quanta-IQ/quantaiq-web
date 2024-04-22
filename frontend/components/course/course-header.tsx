7269e20ae2981bbb485d8cdfab13d3c7c222a5f1
Charles Calapini <7816568+calapsss@users.noreply.github.com>
2024-04-22 09:55:14 -0600

    Update course-header.tsx

--- frontend/components/course/course-header.tsx
+++ frontend/components/course/course-header.tsx
@@ -20,43 +20,43 @@ export default function CourseHead(
     console.log(courseInfo)
 
     return (
-        <>
-        {courseInfo && <div className="mb-4 mt-4 ml-4">
-              <div className="flex flex-row justify-between pb-6">
-                <div>
-                  <h1 className="text-4xl font-bold">{courseInfo.CourseName}</h1>
-                  <p className="text-gray-500 "> {courseInfo.CourseDescription}</p>
-                </div>
-              </div>
-              <Separator />
-              <div className="flex flex-row items-center pt-6 gap-4 ">
-                <Button>
-                  <Link href={`/courses/${courseInfo._id}/syllabus`}>
-                    Syllabus
-                  </Link>
-                </Button>
-                <Button>
-                  <Link href={`/courses/${courseInfo._id}/test`}>
-                    Test
-                  </Link>
-                </Button>
-                <Button>
-                  <Link href={`/courses/${courseInfo._id}/class`}>
-                    Classes
-                  </Link>
-                </Button>
-                <Button>
-                  <Link href={`/courses/${courseInfo._id}/ai`}>
-                    AI Tools
-                  </Link>
-                </Button>
-                <Button>
-                  <Link href={`/courses/${courseInfo._id}/edit`}>
-                    Edit Details
-                  </Link>
-                </Button>
-              </div>
-            </div>}
-        </>
+      <>
+      {courseInfo && <div className="mb-4 mt-4 ml-4">
+          <div className="flex flex-row justify-between pb-6">
+          <div>
+            <h1 className="text-4xl font-bold">{courseInfo.CourseName}</h1>
+            <p className="text-gray-500 "> {courseInfo.CourseDescription}</p>
+          </div>
+          </div>
+          <Separator />
+          <div className="flex flex-row items-center pt-6 gap-4 ">
+          <Button variant={window.location.pathname.includes('/syllabus') ? 'default' : 'secondary'}>
+            <Link href={`/courses/${courseInfo._id}/syllabus`}>
+            Syllabus
+            </Link>
+          </Button>
+          <Button variant={window.location.pathname.includes('/test') ? 'default' : 'secondary'}>
+            <Link href={`/courses/${courseInfo._id}/test`}>
+            Test
+            </Link>
+          </Button>
+          <Button variant={window.location.pathname.includes('/class') ? 'default' : 'secondary'}>
+            <Link href={`/courses/${courseInfo._id}/class`}>
+            Classes
+            </Link>
+          </Button>
+          <Button variant={window.location.pathname.includes('/ai') ? 'default' : 'secondary'}>
+            <Link href={`/courses/${courseInfo._id}/ai`}>
+            AI Tools
+            </Link>
+          </Button>
+          <Button variant={window.location.pathname.includes('/edit') ? 'default' : 'secondary'}>
+            <Link href={`/courses/${courseInfo._id}/edit`}>
+            Edit Details
+            </Link>
+          </Button>
+          </div>
+        </div>}
+      </>
     )
 }

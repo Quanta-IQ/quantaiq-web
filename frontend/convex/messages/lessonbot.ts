573f3a86a7aca58de52bae745563cf90dcf0ea7c
Charles Calapini <7816568+calapsss@users.noreply.github.com>
2024-04-28 18:39:13 -0600

    LessonBot Messages
    - Serves messages

---
+++ frontend/convex/messages/lessonbot.ts
@@ -0,0 +1,50 @@
+import { v } from "convex/values";
+import { mutation } from "../_generated/server";
+import { query } from "../_generated/server";
+import { internal } from "../_generated/api";
+import { Id } from "../_generated/dataModel";
+
+export const list = query({
+  args: {
+    sessionId: v.string(),
+  },
+  handler: async (ctx, args) => {
+    return await ctx.db
+      .query("LessonBotMessages")
+      .withIndex("bySessionId", (q) => q.eq("SessionID", args.sessionId))
+      .collect();
+  },
+});
+
+export const send = mutation({
+  args: {
+    message: v.string(),
+    sessionId: v.string(),
+    lessonId: v.string()
+  },
+  handler: async (ctx, { message, sessionId, lessonId }) => {
+    await ctx.db.insert("LessonBotMessages", {
+      IsViewer: true,      
+      SessionID: sessionId,
+      Text: message,
+      LessonID: lessonId as Id<"Lessons">
+    });
+    await ctx.scheduler.runAfter(0, internal.serve.lessonbot.answer, {
+      sessionId,
+      lessonId
+    });
+  },
+});
+
+export const clear = mutation({
+  args: {
+    sessionId: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const messages = await ctx.db
+      .query("LessonBotMessages")
+      .withIndex("bySessionId", (q) => q.eq("SessionID", args.sessionId))
+      .collect();
+    await Promise.all(messages.map((message) => ctx.db.delete(message._id)));
+  },
+});
